d0d6c60bc2e887aaf52c8c97211930e5cb3906a2
Alex Mickelson <alexmickelson96@gmail.com>
2024-10-14 09:10:59 -0600

    login working

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,35 @@ import { Route, Routes } from "react-router";
 import { PeoplePage } from "./pages/peoplePage/PeoplePage";
 import { OtherPage } from "./pages/OtherPage";
 import { ItemPage } from "./pages/itemApi/ItemPage";
+import { useAuth } from "react-oidc-context";
 
 function App() {
+  const auth = useAuth();
+
+  switch (auth.activeNavigator) {
+    case "signinSilent":
+      return <div>Signing you in...</div>;
+    case "signoutRedirect":
+      return <div>Signing you out...</div>;
+  }
+
+  if (auth.isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (auth.error) {
+    return <div>Oops... {auth.error.message}</div>;
+  }
+
+  if (!auth.isAuthenticated) {
+    return <button onClick={() => void auth.signinRedirect()}>Log in</button>;
+  }
+
+  console.log("user", auth.user);
   return (
     <div>
+      <button onClick={() => auth.signoutRedirect()}>logout</button>
+      <br />
       <header></header>
       <main>
         <nav></nav>
--- src/main.tsx
+++ src/main.tsx
@@ -9,17 +9,29 @@ import { PeopleContextProvider } from "./context/people/PeopleContextProvider.ts
 import { ItemsContextProvider } from "./pages/itemApi/ItemsContextProvider.tsx";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { myQueryClient } from "./services/myQueryClient.ts";
+import { AuthProvider, AuthProviderProps } from "react-oidc-context";
+
+const oidcConfig: AuthProviderProps = {
+  authority: "https://auth.snowse.duckdns.org/realms/advanced-frontend/",
+  client_id: "alex-in-class",
+  redirect_uri: "http://localhost:5173/",
+  // onSigninCallback: () => {
+  //   window.history.replaceState({}, document.title, window.location.pathname);
+  // },
+};
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <QueryClientProvider client={myQueryClient}>
-        <PeopleContextProvider>
-          <ItemsContextProvider>
-            <App />
-          </ItemsContextProvider>
-        </PeopleContextProvider>
-      </QueryClientProvider>
+      <AuthProvider {...oidcConfig}>
+        <QueryClientProvider client={myQueryClient}>
+          <PeopleContextProvider>
+            <ItemsContextProvider>
+              <App />
+            </ItemsContextProvider>
+          </PeopleContextProvider>
+        </QueryClientProvider>
+      </AuthProvider>
     </BrowserRouter>
   </StrictMode>
 );
